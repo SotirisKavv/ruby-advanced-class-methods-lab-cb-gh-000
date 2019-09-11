0ef4738a0af02621c2924f48ad02d5156b49b674
Sotiris Kavvouras <sotos.kavvouras@gmail.com>
2019-09-11 11:45:13 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -6,6 +6,24 @@ class Song
     @@all
   end
 
+  def self.create(name, artist_name)
+    song = self.new
+    song.name = name
+    song.artist_name = artist_name
+    self.save
+    song
+  end
+
+  def self.new_by_name(name)
+    song = self.new
+    song.name = name
+    song
+  end
+
+  def self.create_by_name(name)
+    self.new_by_name(name).save
+    song
+
   def save
     self.class.all << self
   end
